eef26f57c570459a2894fcefbb3c353500039e77
Rocio Garrido <rocio.garrido@wefox.com>
2022-05-25 09:41:50 +0200

    Import the Angular Router

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,25 +1,27 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-
+import { RouterModule } from '@angular/router';
 // Imports for loading & configuring the in-memory web api
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { ProductData } from './products/product-data';
-
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './home/welcome.component';
+import { MessageModule } from './messages/message.module';
 import { PageNotFoundComponent } from './page-not-found.component';
-
+import { ProductData } from './products/product-data';
 /* Feature Modules */
 import { ProductModule } from './products/product.module';
 import { UserModule } from './user/user.module';
-import { MessageModule } from './messages/message.module';
+
+
+
 
 @NgModule({
   imports: [
     BrowserModule,
     HttpClientModule,
     InMemoryWebApiModule.forRoot(ProductData, { delay: 1000 }),
+    RouterModule.forRoot([]),
     ProductModule,
     UserModule,
     MessageModule
