cf6f1ff4bbfa67d2e333b703035e04a93aae0329
Rocio Garrido <rocio.garrido@wefox.com>
2022-06-06 15:22:30 +0200

    Set up for Feature Routing

--- APM-Start/src/app/products/product.module.ts
+++ APM-Start/src/app/products/product.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
-
-import { ProductListComponent } from './product-list.component';
+import { RouterModule } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
 import { ProductDetailComponent } from './product-detail.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
-
-import { SharedModule } from '../shared/shared.module';
+import { ProductListComponent } from './product-list.component';
 
 @NgModule({
   imports: [
-    SharedModule
+    SharedModule,
+    RouterModule.forChild([
+      { path: 'products', component: ProductListComponent }
+    ])
   ],
   declarations: [
     ProductListComponent,
