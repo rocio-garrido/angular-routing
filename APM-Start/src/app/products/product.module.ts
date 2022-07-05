bcdbbef82d6acc6afd7bb59305c2d26674aa37a5
Rocio Garrido <rocio.garrido@wefox.com>
2022-07-05 09:51:18 +0200

    Add a Resolver to a route

--- APM-Start/src/app/products/product.module.ts
+++ APM-Start/src/app/products/product.module.ts
@@ -4,14 +4,22 @@ import { SharedModule } from '../shared/shared.module';
 import { ProductDetailComponent } from './product-detail.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
 import { ProductListComponent } from './product-list.component';
-
+import { ProductResolver } from './product-resolver.service';
 @NgModule({
   imports: [
     SharedModule,
     RouterModule.forChild([
       { path: 'products', component: ProductListComponent },
-      { path: 'products/:id', component: ProductDetailComponent },
-      { path: 'products/:id/edit', component: ProductEditComponent }
+      {
+        path: 'products/:id',
+        component: ProductDetailComponent,
+        resolve: { resolvedData: ProductResolver }
+      },
+      {
+        path: 'products/:id/edit',
+        component: ProductEditComponent,
+        resolve: { resolvedData: ProductResolver }
+      }
     ])
   ],
   declarations: [
