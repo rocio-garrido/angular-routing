9001e051426c9589b9080bf7cb421e8c8633c134
Rocio Garrido <rocio.garrido@wefox.com>
2022-07-25 12:57:04 +0200

    Clearing secondary routes

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -56,6 +56,7 @@ export class AppComponent {
   }
 
   hideMessages(): void {
+    this.router.navigate([{ outlets: { popup: null } }]);
     this.messageService.isDisplayed = false;
   }
 
--- APM-Start/src/app/messages/message.component.ts
+++ APM-Start/src/app/messages/message.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-
 import { MessageService } from './message.service';
+
 
 @Component({
   templateUrl: './message.component.html',
@@ -15,9 +15,11 @@ export class MessageComponent {
   }
 
   constructor(private messageService: MessageService,
-              private router: Router) { }
+    private router: Router) { }
 
   close(): void {
     // Close the popup.
+    this.router.navigate([{ outlets: { popup: null } }]);
+    this.messageService.isDisplayed = false;
   }
 }
