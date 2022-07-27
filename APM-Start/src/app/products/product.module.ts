f80c1244cdec22dd4fe87d7a930b151166b86cb1
Rocio Garrido <rocio.garrido@wefox.com>
2022-07-27 10:13:02 +0200

    CanActivated Guard

--- APM-Start/src/app/products/product.module.ts
+++ APM-Start/src/app/products/product.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
+import { AuthGuard } from '../user/auth.guard';
 import { ProductDetailComponent } from './product-detail.component';
 import { ProductEditInfoComponent } from './product-edit/product-edit-info.component';
 import { ProductEditTagsComponent } from './product-edit/product-edit-tags.component';
@@ -13,6 +14,7 @@ import { ProductResolver } from './product-resolver.service';
     RouterModule.forChild([
       {
         path: 'products',
+        canActivate: [AuthGuard],
         children: [
           {
             path: '',
---
+++ APM-Start/src/app/user/auth.guard.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private authService: AuthService,
+    private router: Router) {
+
+  }
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.checkLoggedIn();
+  }
+
+  checkLoggedIn(): boolean {
+    if (this.authService.isLoggedIn) {
+      return true;
+    }
+
+    // Retain the attempted URL for redirection
+    this.router.navigate(['/login']);
+    return false;
+  }
+
+}
