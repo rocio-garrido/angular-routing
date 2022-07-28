ed8d3f6ec7ac81261952a2734f0082e85e1461c1
Rocio Garrido <rocio.garrido@wefox.com>
2022-07-28 12:37:49 +0200

    Lazy Loading

--- APM-Start/src/app/app-routing.module.ts
+++ APM-Start/src/app/app-routing.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { WelcomeComponent } from './home/welcome.component';
 import { PageNotFoundComponent } from './page-not-found.component';
+import { AuthGuard } from './user/auth.guard';
 
 
 @NgModule({
     imports: [
         RouterModule.forRoot([
             { path: 'welcome', component: WelcomeComponent },
+            {
+                path: 'products',
+                canActivate: [AuthGuard],
+                loadChildren: () =>
+                    import('./products/product.module').then(m => m.ProductModule)
+            },
             { path: '', redirectTo: 'welcome', pathMatch: 'full' },
             { path: '**', component: PageNotFoundComponent }
         ]),
--- APM-Start/src/app/products/product.module.ts
+++ APM-Start/src/app/products/product.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ProductEditGuard } from '../products/product-edit/product-edit.guard';
 import { SharedModule } from '../shared/shared.module';
-import { AuthGuard } from '../user/auth.guard';
 import { ProductDetailComponent } from './product-detail.component';
 import { ProductEditInfoComponent } from './product-edit/product-edit-info.component';
 import { ProductEditTagsComponent } from './product-edit/product-edit-tags.component';
@@ -14,29 +13,23 @@ import { ProductResolver } from './product-resolver.service';
     SharedModule,
     RouterModule.forChild([
       {
-        path: 'products',
-        canActivate: [AuthGuard],
+        path: '',
+        component: ProductListComponent
+      },
+      {
+        path: ':id',
+        component: ProductDetailComponent,
+        resolve: { resolvedData: ProductResolver }
+      },
+      {
+        path: ':id/edit',
+        component: ProductEditComponent,
+        canDeactivate: [ProductEditGuard],
+        resolve: { resolvedData: ProductResolver },
         children: [
-          {
-            path: '',
-            component: ProductListComponent
-          },
-          {
-            path: ':id',
-            component: ProductDetailComponent,
-            resolve: { resolvedData: ProductResolver }
-          },
-          {
-            path: ':id/edit',
-            component: ProductEditComponent,
-            canDeactivate: [ProductEditGuard],
-            resolve: { resolvedData: ProductResolver },
-            children: [
-              { path: '', redirectTo: 'info', pathMatch: 'full' },
-              { path: 'info', component: ProductEditInfoComponent },
-              { path: 'tags', component: ProductEditTagsComponent }
-            ]
-          }
+          { path: '', redirectTo: 'info', pathMatch: 'full' },
+          { path: 'info', component: ProductEditInfoComponent },
+          { path: 'tags', component: ProductEditTagsComponent }
         ]
       }
     ])
